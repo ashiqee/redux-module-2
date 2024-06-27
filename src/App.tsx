39cde17091a5b3435e538d4eeecb6353c04c6f1d
ashiqee <ashiq.buet73@gmail.com>
2024-06-28 00:34:49 +0600

    counterSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useDispatch, useSelector } from "react-redux"
-import { decrement, increment } from "./redux/features/counterSlice"
+
+import { decrement, increment, incrementByValue } from "./redux/features/counterSlice"
+import { useAppDispatch, useAppSelector } from "./redux/hook"
+
 
 
 function App() {
-  const {count} = useSelector((state)=> state.counter)
-  const dispatch = useDispatch()
+  const {count} = useAppSelector((state)=> state.counter)
+  const dispatch = useAppDispatch()
 
 
   return (
@@ -16,6 +18,9 @@ function App() {
  className=" px-3 py-2 rounded-md bg-green-800 text-white">Increment</button>
  <h1 className=" text-3xl mx-10">{count}</h1>
  <button  onClick={()=>dispatch(decrement())} className=" px-3 py-2 rounded-md bg-red-800 text-white">Decrement</button>
+ <button  onClick={()=>dispatch(incrementByValue(5))} 
+ className=" px-3 py-2 rounded-md bg-red-800 text-white">
+  increment by 5</button>
 </div>
     </div>
   )
